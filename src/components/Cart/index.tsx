ee12bc685346205e1cc42eb65d7fabcc4c259022
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-07-16 16:08:19 -0300

    feat: creating validation to form route

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import Button from '../Button'
 import Tag from '../Tag'
@@ -7,11 +8,12 @@ import { RootReducer } from '../../store'
 import { close, remove } from '../../store/reducers/cart'
 
 import * as S from './styles'
-import { parseToBrl } from '../../utils'
+import { getTotalPrice, parseToBrl } from '../../utils'
 
 const Cart = () => {
   const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const closeCart = () => {
     dispatch(close())
@@ -21,10 +23,9 @@ const Cart = () => {
     dispatch(remove(id))
   }
 
-  const getTotalPrice = () => {
-    return items.reduce((previous, current) => {
-      return (previous += current.prices.current!)
-    }, 0)
+  const goToCheckout = () => {
+    navigate('/checkout')
+    closeCart()
   }
   return (
     <S.CartContainer className={isOpen ? 'is-open' : ''}>
@@ -46,10 +47,14 @@ const Cart = () => {
         </ul>
         <S.Quantity>{items.length} jogo(s) no carrinho</S.Quantity>
         <S.Prices>
-          Total de {parseToBrl(getTotalPrice())}{' '}
+          Total de {parseToBrl(getTotalPrice(items))}{' '}
           <span>Em até 6x sem juros</span>
         </S.Prices>
-        <Button title="Clique aqui para continuar com a compra" type="button">
+        <Button
+          title="Clique aqui para continuar com a compra"
+          type="button"
+          onClick={goToCheckout}
+        >
           Continuar com a compra
         </Button>
       </S.SideBar>
